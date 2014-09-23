8e8fd11e4694ee0fdb784a95bfc510d7c121cc0f
Donald Plummer <dplummer@avvo.com>
2014-09-23 13:48:23 -0700

    Refactor to reduce abstraction

--- lib/json_api_client/scope.rb
+++ lib/json_api_client/scope.rb
@@ -45,7 +45,20 @@ module JsonApiClient
     end
 
     def find_each(&block)
-      Query::FindEach.new(klass, params).call(&block)
+      each_page do |results|
+        results.each(&block)
+      end
+    end
+
+    private
+
+    def each_page(&block)
+      page = 1
+      while page
+        results = klass.page(page).where(params).to_a
+        block.call(results)
+        page = results.next_page
+      end
     end
 
   end
