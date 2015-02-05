1c3dae7e9d0abed571ff666775a461ddc0b81f76
Jeff Ching <ching.jeff@gmail.com>
2015-02-04 23:04:59 -0800

    failing test for handling meta => page (instead of current_page)

--- test/unit/pagination_test.rb
+++ test/unit/pagination_test.rb
@@ -125,4 +125,30 @@ class PaginationTest < MiniTest::Unit::TestCase
     assert_equal 3, users.per_page
   end
 
+  def test_can_handle_page_param
+    stub_request(:get, "http://localhost:3000/api/1/users.json")
+    .to_return(headers: {content_type: "application/json"}, body: {
+      users: [
+        {id: 1, name: "Jeff Ching", email_address: "ching.jeff@gmail.com"},
+        {id: 2, name: "Barry Bonds", email_address: "barry@bonds.com"},
+        {id: 3, name: "Hank Aaron", email_address: "hank@aaron.com"}
+      ],
+      meta: {
+        per_page: 3,
+        page: 2,
+        offset: 3,
+        total_entries: 10,
+        total_pages: 4
+      }
+    }.to_json)
+
+    users = User.all
+    assert_equal 3, users.length
+    assert_equal 3, users.per_page
+    assert_equal 2, users.current_page
+    assert_equal 3, users.offset
+    assert_equal 10, users.total_entries
+    assert_equal 4, users.total_pages
+  end
+
 end
