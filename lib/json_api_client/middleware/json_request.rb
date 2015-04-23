be06cbcefe7a72497c636a1fbd1d391d3ff58ea8
Jeff Ching <ching.jeff@gmail.com>
2015-04-23 13:34:17 -0400

    request the correct content-type for json api spec. Fixes #31

--- lib/json_api_client/middleware/json_request.rb
+++ lib/json_api_client/middleware/json_request.rb
@@ -2,7 +2,7 @@ module JsonApiClient
   module Middleware
     class JsonRequest < Faraday::Middleware
       def call(environment)
-        environment[:request_headers]["Accept"] = "application/json,*/*"
+        environment[:request_headers]["Accept"] = "application/vnd.api+json,application/json,*/*"
         uri = environment[:url]
         uri.path = uri.path + ".json" unless uri.path.match(/\.json$/) 
         @app.call(environment)
