0a335eedbffaa7d7c19a0848a8adc77ddc8bdbc3
Jeff Ching <ching.jeff@gmail.com>
2015-04-23 16:13:04 -0400

    only add application/vnd.api+json as the Accept header

--- lib/json_api_client/middleware/json_request.rb
+++ lib/json_api_client/middleware/json_request.rb
@@ -2,7 +2,7 @@ module JsonApiClient
   module Middleware
     class JsonRequest < Faraday::Middleware
       def call(environment)
-        environment[:request_headers]["Accept"] = "application/vnd.api+json,application/json,*/*"
+        environment[:request_headers]["Accept"] = "application/vnd.api+json"
         uri = environment[:url]
         uri.path = uri.path + ".json" unless uri.path.match(/\.json$/) 
         @app.call(environment)
