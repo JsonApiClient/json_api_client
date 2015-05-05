d5f5e2c40d593a81a571282631d16c3146e5892c
Jeff Ching <jching@avvo.com>
2015-05-05 09:50:51 -0700

    remove unused code

--- lib/json_api_client/result_set.rb
+++ lib/json_api_client/result_set.rb
@@ -14,16 +14,6 @@ module JsonApiClient
     # pagination methods are handled by the paginator
     def_delegators :pages, :total_pages, :total_entries, :offset, :per_page, :current_page, :limit_value, :next_page, :previous_page, :out_of_bounds?
 
-    def self.build(klass, data)
-      # Objects representing an individual resource are
-      # not necessarily wrapped in an Array; enforce wrapping
-      result_data = [data.fetch(klass.table_name, [])].flatten
-      new(result_data.map {|attributes| klass.new(attributes) }).tap do |result_set|
-        result_set.record_class = klass
-        yield(result_set) if block_given?
-      end
-    end
-
     def has_errors?
       errors && errors.length > 0
     end
