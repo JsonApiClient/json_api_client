ad8195783ab3842cdffb17007836182bf5a9dd63
Jeff Ching <jching@avvo.com>
2015-07-13 13:10:51 -0700

    add test for trying to fire off requests in different threads

--- lib/json_api_client/helpers/custom_headers.rb
+++ lib/json_api_client/helpers/custom_headers.rb
@@ -7,17 +7,20 @@ module JsonApiClient
         def with_headers(headers)
           self.custom_headers = headers
           yield
+        ensure
           self.custom_headers = {}
         end
 
         def custom_headers
-          key = "json_api_client-#{resource_name}"
-          Thread.current[key] ||= {}
+          self.header_store
         end
 
         def custom_headers=(headers)
-          key = "json_api_client-#{resource_name}"
-          Thread.current[key] = headers
+          self.header_store.replace(headers)
+        end
+
+        def header_store
+          Thread.current["json_api_client-#{resource_name}"] ||= {}
         end
       end
 
--- test/unit/custom_header_test.rb
+++ test/unit/custom_header_test.rb
@@ -27,4 +27,51 @@ class CustomHeaderTest < MiniTest::Test
     end
   end
 
+  def test_multiple_threads
+    thread_count = 10
+
+    # set up expectations/stubs
+    thread_count.times do |i|
+      stub_request(:get, "http://example.com/custom_header_resources/#{i}")
+        .with(headers: {"X-My-Header" => "Header #{i}"})
+        .to_return(headers: {content_type: "application/vnd.api+json"}, body: {
+          data: {
+            type: "custom_header_resources",
+            id: "#{i}",
+            attributes: {
+              title: "Rails is Omakase"
+            }
+          }
+        }.to_json)
+    end
+
+    @ready = false
+    threads = []
+
+    # create threads and set up to wait and try and fire them off at the same time
+    thread_count.times do |i|
+      threads << Thread.new do
+        CustomHeaderResource.with_headers(x_my_header: "Header #{i}") do
+          wait_for { @ready }
+          resources = CustomHeaderResource.find(i)
+        end
+      end
+    end
+
+    @ready = true
+
+    threads.each_with_index do |thread, i|
+      resource = thread.value.first
+      assert_equal i, resource.id.to_i
+    end
+  end
+
+  protected
+
+  def wait_for
+    Timeout.timeout 1 do
+      sleep 0.001 until yield
+    end
+  end
+
 end
