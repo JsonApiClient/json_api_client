2208fdfe59b4f896104ce2a5b67083184cb5288b
Seth Ringling <sringling@avvo.com>
2015-07-15 12:45:30 -0700

    If the page param is invalid, default it instead of passing on the bad value. A spambot might send page=abc to your pages, and we should handle that better.  Even page=0 will fail.

--- test/unit/scope_test.rb
+++ test/unit/scope_test.rb
@@ -20,4 +20,18 @@ class ScopeTest < MiniTest::Unit::TestCase
     assert_equal({a: "b", c: "d", e: "f", order: "name desc", per_page: 30, page: 2}, scope.params)
   end
 
+  def test_invalid_pages
+    scope = JsonApiClient::Scope.new(User)
+    assert_equal(1, scope.page(1).params[:page])
+    assert_equal(1, scope.page("1").params[:page])
+    assert_equal(88, scope.page("88").params[:page])
+    assert_equal(1, scope.page("abc").params[:page])
+    assert_equal(1, scope.page("-1").params[:page])
+    assert_equal(1, scope.page("0").params[:page])
+    assert_equal(1, scope.page(0).params[:page])
+    assert_equal(1, scope.page(-1).params[:page])
+    assert_equal(1, scope.page(-99).params[:page])
+    assert_equal(15, scope.page(15).params[:page])
+  end
+
 end
