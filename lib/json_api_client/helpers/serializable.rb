9eb3bd84c7f3b46aa1e35a2c6427b7905adb0b3c
Denis Talakevich <denis.t@didww.com>
2015-07-22 17:46:33 +0300

    remove relationships_for_serialization from Helpers::Serializable

--- lib/json_api_client/helpers/serializable.rb
+++ lib/json_api_client/helpers/serializable.rb
@@ -6,14 +6,11 @@ module JsonApiClient
       included do
         class_attribute :read_only_attributes, instance_accessor: false
         self.read_only_attributes = ['id', 'type', 'links', 'meta', 'relationships']
-
-        class_attribute :read_only_relations, instance_accessor: false
-        self.read_only_relations = []
       end
 
       def serializable_hash
         attributes.slice('id', 'type').tap do |h|
-          relations_for_serialization.tap do |r|
+          relationships.serializable_hash.tap do |r|
             h['relationships'] = r unless r.empty?
           end
           h['attributes'] = attributes_for_serialization
@@ -29,10 +26,6 @@ module JsonApiClient
 
       def attributes_for_serialization
         attributes.except(*self.class.read_only_attributes).slice(*changed)
-      end
-
-      def relations_for_serialization
-        relationships.serializable_hash.except(*self.class.read_only_relations)
       end
 
     end
