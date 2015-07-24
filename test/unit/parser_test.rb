4031824ea23422f5a062118334850fa6f2e40090
Seth Ringling <sringling@avvo.com>
2015-07-24 10:52:11 -0700

    Test for indifferent metadata access

--- test/unit/parser_test.rb
+++ test/unit/parser_test.rb
@@ -6,4 +6,30 @@ class ParserTest < MiniTest::Unit::TestCase
     assert JsonApiClient::Parser.is_a?(Class)
   end
 
+  def test_meta_is_accessible_indifferently
+    result = JsonApiClient::ResultSet.new
+    args = [result, mock_response]
+    JsonApiClient::Parser.send('handle_meta', *args)
+    assert_equal(200, result.meta["status"])
+    assert_equal(200, result.meta[:status])
+    assert_equal(1, result.meta["page"])
+    assert_equal(1, result.meta[:page])
+    assert_equal(9999, result.meta["total_pages"])
+    assert_equal(9999, result.meta[:total_pages])
+  end
+
+  private
+
+  def mock_response
+    {
+        "meta": {
+            "per-page": 10,
+            "page": 1,
+            "status": 200,
+            "total_pages": 9999,
+            "errors": nil
+        }
+    }.stringify_keys!
+  end
+
 end
