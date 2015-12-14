1bb12fe06c2bdcf7d8be60d48d257956ca6565cb
Donald Plummer <dplummer@avvo.com>
2015-12-14 09:23:36 -0800

    Allow linked resources to have links
    So if a linked resource has its own links, you can access those.

--- test/unit/links_test.rb
+++ test/unit/links_test.rb
@@ -69,15 +69,27 @@ class LinksTest < MiniTest::Unit::TestCase
           "user.address" => {
             href: 'http://localhost:3000/api/1/addresses/{user.address}',
             type: "addresses"
+          },
+          "address.state" => {
+            href: 'http://localhost:3000/api/1/states/{address.state}',
+            type: "states"
           }
         },
         linked: {
           addresses: [
-            {id: 4, address: "1st Ave S"}
+            {
+              id: 4, address: "1st Ave S", state_id: 1,
+              links: {
+                "state": 1
+              }
+            }
           ],
           posts: [
             {id: 2, title: "Yo", body: "Lo"},
             {id: 3, title: "Foo", body: "Bar"}
+          ],
+          states: [
+            {id: 1, name: "Washington"}
           ]
         }
       }.to_json)
@@ -97,6 +109,10 @@ class LinksTest < MiniTest::Unit::TestCase
     assert_equal 1, address.length
     assert address.first.is_a?(Address), "should figure out what type of object for Address"
 
+    assert address.first.respond_to?(:state), "expected deep-linked state to be available"
+    state = address.first.state.first
+    assert state.is_a?(State), "should figure out what type of object for State"
+    assert_equal "Washington", state.name, "should load data from linked section for state"
   end
 
 end
