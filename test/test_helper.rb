a821205ee4fe0c05df99c8957f9b4cfa5267de16
x4d3 <git@xade.eu>
2017-01-11 14:33:58 +0000

    Fix CodeClimate issue
    Folllowing guidance from https://github.com/codeclimate/ruby-test-reporter/blob/master/CHANGELOG.md#v100-2016-11-03

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 Bundler.require(:default, :test)
 require 'minitest/autorun'
 require 'webmock/minitest'
