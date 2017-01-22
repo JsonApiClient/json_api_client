e82298c50cde036c829c0d764de5bed3abeff4b1
Ben Dunham <ben@jibboom.co.uk>
2017-01-22 10:46:08 +0000

    Added test for key formatted attribute error messages

--- test/unit/server_side_error_test.rb
+++ test/unit/server_side_error_test.rb
@@ -32,4 +32,18 @@ class ServerSideErrorTest < MiniTest::Test
     assert_equal ["Email address is invalid"], user.errors.full_messages
   end
 
+  def test_can_handle_key_formatted_attribute_validation_strings
+    with_altered_config(User, :json_key_format => :dasherized_key) do
+      stub_request(:post, "http://example.com/users")
+        .to_return(headers: {content_type: "application/vnd.api+json"}, body: {
+          errors: [{source: {pointer: "/data/attributes/email-address"}, title: "Email address is invalid"}]
+        }.to_json)
+      
+      user = User.create(name: 'Bob', email_address: 'invalid email')
+      assert !user.persisted?
+      assert user.errors.present?
+      assert_equal ["Email address is invalid"], user.errors[:email_address]
+    end
+  end
+
 end
