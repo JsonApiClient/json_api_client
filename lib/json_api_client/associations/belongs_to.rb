b987aa7bcd420bd7d481aeceb40d7b4e17899392
Riley Trautman <rileyt@spiceworks.com>
2017-06-21 17:42:21 -0500

    Format belongs_to URLs

--- lib/json_api_client/associations/belongs_to.rb
+++ lib/json_api_client/associations/belongs_to.rb
@@ -16,13 +16,13 @@ module JsonApiClient
           :"#{attr_name}_id"
         end
 
-        def to_prefix_path
-          "#{attr_name.to_s.pluralize}/%{#{param}}"
+        def to_prefix_path(formatter)
+          "#{formatter.format(attr_name.to_s.pluralize)}/%{#{param}}"
         end
 
-        def set_prefix_path(attrs)
+        def set_prefix_path(attrs, formatter)
           attrs[param] = encode_part(attrs[param]) if attrs.key?(param)
-          to_prefix_path % attrs
+          to_prefix_path(formatter) % attrs
         end
       end
     end
