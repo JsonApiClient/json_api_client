c19c21b7830a30d692f213981aedd4573a621fe2
Terry Wilson <terry.wilson@twss.co.uk>
2017-11-22 17:32:32 +0000

    Allow custom ‘Accept’ header, prepends ‘application/vnd.api+json’ if it is missing. (#245)

--- lib/json_api_client/middleware/json_request.rb
+++ lib/json_api_client/middleware/json_request.rb
@@ -2,9 +2,24 @@ module JsonApiClient
   module Middleware
     class JsonRequest < Faraday::Middleware
       def call(environment)
+        accept_header = update_accept_header(environment[:request_headers])
+
         environment[:request_headers]["Content-Type"] = 'application/vnd.api+json'
-        environment[:request_headers]["Accept"] = 'application/vnd.api+json'
+        environment[:request_headers]["Accept"] = accept_header
         @app.call(environment)
+      end
+
+      private
+
+      def update_accept_header(headers)
+        return 'application/vnd.api+json' if headers["Accept"].nil?
+        accept_params = headers["Accept"].split(",")
+
+        unless accept_params.include?('application/vnd.api+json')
+          accept_params.unshift('application/vnd.api+json')
+        end
+
+        accept_params.join(",")
       end
     end
   end
