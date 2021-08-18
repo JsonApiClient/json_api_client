fc5983836519dda846969f9ab62a61bc38778ba8
Tilo Sloboda <tsloboda@chime.com>
2021-08-18 13:47:08 -0700

    adding Pry as a development dependency

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require 'minitest/autorun'
 require 'webmock/minitest'
 require 'mocha/minitest'
 require 'pp'
+require 'pry'
 
 # shim for ActiveSupport 4.0.x requiring minitest 4.2
 unless defined?(Minitest::Test)
